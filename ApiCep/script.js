bc1eaf70bcb8bac460bd36f12921dfddd47e0fe3
Lucas <lucasrian.dev@gmail.com>
2025-12-21 23:44:35 -0300

    Treinando Api

---
+++ ApiCep/script.js
@@ -0,0 +1,39 @@
+const cepInput = document.querySelector("#cep-input");
+const botao = document.querySelector("#btn-buscar");
+
+const inputRua = document.querySelector("#rua");
+const inputBairro = document.querySelector("#bairro");
+const inputCidade = document.querySelector("#cidade");
+
+botao.addEventListener("click", async () => {
+  try {
+    const valorDoCep = cepInput.value;
+
+    if (valorDoCep.length !== 8) {
+      alert("O CEP precisa ter 8 dígitos!");
+      return;
+    }
+
+    const resposta = await fetch(
+      `https://viacep.com.br/ws/${valorDoCep}/json/`
+    );
+
+    if (!resposta.ok) {
+      throw new Error("Erro no servidor");
+    }
+
+    const dados = await resposta.json();
+
+    if (dados.erro) {
+      alert("CEP não encontrado!");
+      return;
+    }
+
+    inputRua.value = dados.logradouro;
+    inputBairro.value = dados.bairro;
+    inputCidade.value = dados.localidade + "/" + dados.uf;
+  } catch (error) {
+    console.log(error);
+    alert("Deu algo errado na busca. Verifique o CEP.");
+  }
+});
