c041d6d74e82b6221a7d5de69c6d78186d1ed13e
Lucas <lucasrian.dev@gmail.com>
2025-12-24 17:31:17 -0300

    ajustando logica

--- ApiPikachu/script.js
+++ ApiPikachu/script.js
@@ -1,32 +1,36 @@
-const input = document.querySelector('#busca')
-const botao = document.querySelector('#btn-buscar')
+const input = document.querySelector("#busca");
+const botao = document.querySelector("#btn-buscar");
 
-let imagem = document.querySelector('img')
-let nome1 = document.querySelector('#poke-name')
-let tipo = document.querySelector('.info')
+let imagem = document.querySelector("img");
+let nome1 = document.querySelector("#poke-name");
+let tipo = document.querySelector(".info");
 
+async function carregarPokemon() {
+  const nome = input.value.toLowerCase();
 
-botao.addEventListener('click', async()=>{
-    const nome = input.value.toLowerCase()
+  try {
+    const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
 
-    try {
-        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+    if (!resposta.ok) {
+      throw new Error("Algum erro em trazer api");
+    }
 
-        if(!resposta.ok){
-            throw new Error("Algum erro em trazer api");
-            
-        }
+    const dados = await resposta.json();
 
-        const dados = await resposta.json()
-        console.log(dados)
+    imagem.src = dados.sprites.front_default;
+    nome1.innerText = dados.name;
+    tipo.innerText = dados.types[0].type.name;
 
-        imagem.src = dados.sprites.front_default
-        nome1.innerText = dados.name
-        tipo.innerText = dados.types[0].type.name
-        
+    input.value = "";
+  } catch (error) {
+    console.log("Ops deu erro", error);
+  }
+}
 
+botao.addEventListener("click", carregarPokemon);
 
-    } catch (error) {
-        console.log('Ops deu erro', error)
-    } 
-})
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    carregarPokemon();
+  }
+});
