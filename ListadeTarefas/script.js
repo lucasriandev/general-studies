e40cb0c291a5cb345b882fd4aba4e5628b0d1d84
Lucas <lucasrian.dev@gmail.com>
2026-01-09 16:19:55 -0300

    js concluido

--- ListadeTarefas/script.js
+++ ListadeTarefas/script.js
@@ -1,7 +1,47 @@
-const input = document.querySelector("#input-group");
+const input = document.querySelector("#input-tarefa");
 const adicionar = document.querySelector("#btn-add");
 const lista = document.querySelector("#lista");
 
 let minhaLista = [];
 
-function desenharTarefas(texto) {}
+const listaguardada = localStorage.getItem("lista");
+
+if (listaguardada) {
+  minhaLista = JSON.parse(listaguardada);
+}
+
+minhaLista.forEach((item) => {
+  desenharTarefas(item);
+});
+
+function desenharTarefas(texto) {
+  const novoLi = document.createElement("li");
+  novoLi.innerText = texto;
+
+  const novoBtn = document.createElement("button");
+  novoBtn.innerText = "❌";
+  novoBtn.classList.add("btn-delete");
+
+  novoBtn.addEventListener("click", () => {
+    novoLi.remove();
+    const posicao = minhaLista.indexOf(texto);
+
+    if (posicao !== -1) {
+      minhaLista.splice(posicao, 1);
+    }
+
+    localStorage.setItem("lista", JSON.stringify(minhaLista));
+  });
+
+  novoLi.appendChild(novoBtn);
+  lista.appendChild(novoLi);
+}
+
+adicionar.addEventListener("click", () => {
+  const valor = input.value;
+
+  minhaLista.push(valor);
+  desenharTarefas(valor);
+
+  localStorage.setItem("lista", JSON.stringify(minhaLista));
+});
