5813daa861d275f66e650ac51c1ca0e291f4dea3
Lucas <lucasrian.dev@gmail.com>
2026-01-09 14:27:41 -0300

    js ainda faltando

---
+++ ListadeTarefas/script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#input-group");
+const adicionar = document.querySelector("#btn-add");
+const lista = document.querySelector("#lista");
+
+let minhaLista = [];
+
+function desenharTarefas(texto) {}
