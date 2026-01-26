f075ec35f70667c05cf3b2d46750438a0d7f6489
Lucas <lucasrian.dev@gmail.com>
2026-01-26 13:40:15 -0300

    Carrinho add: Botao apagar, localStorage

--- Carrinho/script.js
+++ Carrinho/script.js
@@ -1,19 +1,51 @@
-const input = document.querySelector('#input-preco')
-const lista = document.querySelector('#lista-produtos')
-const span = document.querySelector('#valor-total')
-const botao = document.querySelector('#btn-add')
+const input = document.querySelector("#input-preco");
+const lista = document.querySelector("#lista-produtos");
+const span = document.querySelector("#valor-total");
+const botao = document.querySelector("#btn-add");
 
-let carrinho = []
+let carrinho = JSON.parse(localStorage.getItem("ListaCarrinho")) || [];
 
-botao.addEventListener('click', ()=>{
-    const valor = Number(input.value)
-    carrinho.push(valor)
-    lista.innerHTML += `<li>R$ ${valor}</li>`
-    
-    let soma = 0
-    carrinho.forEach((item)=>{
-        soma = soma + item
-    })
-    span.innerText = soma.toFixed(2) 
-    input.value = ''
-})
+function resultado() {
+  lista.innerHTML = "";
+
+  let soma = 0;
+
+  carrinho.forEach((item, index) => {
+    const novoLI = document.createElement("li");
+    novoLI.innerHTML = `R$ ${item.toFixed(2)}`;
+    lista.appendChild(novoLI);
+
+    const apagar = document.createElement("button");
+    apagar.innerHTML = "X";
+    apagar.classList.add("btn-delete");
+
+    apagar.addEventListener("click", () => {
+      carrinho.splice(index, 1);
+      localStorage.setItem("ListaCarrinho", JSON.stringify(carrinho));
+      resultado();
+    });
+
+    novoLI.appendChild(apagar);
+
+    soma = soma + item;
+  });
+
+  span.innerText = soma.toFixed(2);
+  input.value = "";
+}
+
+resultado();
+
+botao.addEventListener("click", () => {
+  const valor = Number(input.value);
+  carrinho.push(valor);
+
+  localStorage.setItem("ListaCarrinho", JSON.stringify(carrinho));
+  resultado();
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    botao.click();
+  }
+});
