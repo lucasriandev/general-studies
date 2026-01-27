b19e35945196f3bcacb0a0c11c631ccbe012b4a1
Lucas <lucasrian.dev@gmail.com>
2026-01-27 12:09:14 -0300

    Comanda de bar add: localstorage

--- ComandaDeBar/script.js
+++ ComandaDeBar/script.js
@@ -1,14 +1,20 @@
-let total = 0;
 const visor = document.querySelector("#visor");
+
+let total = JSON.parse(localStorage.getItem("Comanda")) || 0;
+
+visor.innerHTML = `R$` + total.toFixed(2);
 
 function adicionar(taxa) {
   total = total + taxa;
 
   visor.innerHTML = "R$" + total.toFixed(2);
   console.log(total);
+
+  localStorage.setItem("Comanda", JSON.stringify(total));
 }
 
 function zerar() {
   total = 0;
   visor.innerHTML = "R$" + total.toFixed(2);
+  localStorage.removeItem("Comanda");
 }
