57c65bb0f2e10979a05eaabbca85a19aba754ea6
Lucas <lucasrian.dev@gmail.com>
2026-01-29 17:38:55 -0300

    Finalizado FeedComentarios com localstorage

--- FeedComentarios/script.js
+++ FeedComentarios/script.js
@@ -14,5 +14,44 @@ function renderizar() {
 
     const h3 = document.createElement("h3");
     h3.innerText = item.usuario;
+
+    const p = document.createElement("p");
+    p.innerText = item.conteudo;
+
+    const btnDelete = document.createElement("button");
+    btnDelete.innerText = "❌";
+
+    btnDelete.addEventListener("click", () => {
+      comentarios.splice(index, 1);
+      localStorage.setItem("Novidades", JSON.stringify(comentarios));
+      renderizar();
+    });
+
+    novaDiv.append(h3, p, btnDelete);
+    feed.appendChild(novaDiv);
   });
 }
+
+publicar.addEventListener("click", () => {
+  if (inputNome.value === "" || inputMsg.value === "") {
+    alert("Campos vazios!");
+    return;
+  }
+
+  const novoComentarios = {
+    usuario: inputNome.value,
+    conteudo: inputMsg.value,
+  };
+
+  comentarios.push(novoComentarios);
+  localStorage.setItem("Novidades", JSON.stringify(comentarios));
+
+  inputNome.value = "";
+  inputMsg.value = "";
+
+  renderizar();
+});
+
+renderizar();
+
+localStorage.clear();
