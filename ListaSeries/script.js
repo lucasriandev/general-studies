b6d125a6ac6ff4706b4fab6781c3e0da279e566f
Lucas <lucasrian.dev@gmail.com>
2026-02-01 15:59:17 -0300

    ListadeSeries refeita

--- ListaSeries/script.js
+++ ListaSeries/script.js
@@ -1,3 +1,47 @@
-const input = document.querySelector("input-serie");
-const salvar = document.querySelector("btn-add");
-const ul = document.querySelector("lista");
+const input = document.querySelector("#input-serie");
+const salvar = document.querySelector("#btn-add");
+const ul = document.querySelector("#lista");
+
+let minhasSeries = JSON.parse(localStorage.getItem("MinhasSeries")) || [];
+minhasSeries = Array.isArray(minhasSeries) ? minhasSeries : [];
+
+function salvarSeries() {
+  const valor = input.value;
+  minhasSeries.push(valor);
+  localStorage.setItem("MinhasSeries", JSON.stringify(minhasSeries));
+
+  input.value = "";
+
+  renderizarSeries();
+}
+
+function renderizarSeries() {
+  ul.innerHTML = "";
+
+  minhasSeries.forEach((item, index) => {
+    const novoLi = document.createElement("li");
+    novoLi.innerText = item;
+
+    const apagar = document.createElement("button");
+    apagar.innerText = "❌";
+
+    apagar.addEventListener("click", () => {
+      minhasSeries.splice(index, 1);
+      localStorage.setItem("MinhasSeries", JSON.stringify(minhasSeries));
+      renderizarSeries();
+    });
+
+    ul.append(novoLi, apagar);
+  });
+}
+
+renderizarSeries();
+
+salvar.addEventListener("click", salvarSeries);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    salvarSeries();
+  }
+});
