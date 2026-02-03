d6ddee55f6f9d358dae6e29bfb1950f1840033c8
Lucas <lucasrian.dev@gmail.com>
2026-02-03 13:50:58 -0300

    Add localstorage em AnalisadorNumeros

--- AnalisadorNumeros/script.js
+++ AnalisadorNumeros/script.js
@@ -1,45 +1,47 @@
-const input = document.querySelector('#input-numero')
-const listavisual = document.querySelector('#lista-visual')
-const spanMaior = document.querySelector('#res-maior')
-const spanMenor = document.querySelector('#res-menor')
+const input = document.querySelector("#input-numero");
+const listavisual = document.querySelector("#lista-visual");
+const spanMaior = document.querySelector("#res-maior");
+const spanMenor = document.querySelector("#res-menor");
 
-let listaNumeros = []
+let listaNumeros = JSON.parse(localStorage.getItem("Numeros")) || [];
+listaNumeros = Array.isArray(listaNumeros) ? listaNumeros : [];
 
-const botaoAdd = document.querySelector('#btn-add')
+listavisual.innerText = listaNumeros.join(", ");
 
-botaoAdd.addEventListener('click', ()=>{
-    const valor = Number(input.value)
-    
-    if(input.value === '') {
-        alert("Digite um número!")
-        return
+const botaoAdd = document.querySelector("#btn-add");
+
+botaoAdd.addEventListener("click", () => {
+  const valor = Number(input.value);
+
+  if (input.value === "") {
+    alert("Digite um número!");
+    return;
+  }
+
+  listaNumeros.push(valor);
+  listavisual.innerText = listaNumeros.join(", ");
+  localStorage.setItem("Numeros", JSON.stringify(listaNumeros));
+  input.value = "";
+});
+
+const botaoAnalisar = document.querySelector("#btn-analisar");
+
+botaoAnalisar.addEventListener("click", () => {
+  if (listaNumeros.length === 0) {
+    alert("Lista vazia");
+    return;
+  }
+
+  let maior = listaNumeros[0];
+  let menor = listaNumeros[0];
+
+  listaNumeros.forEach((item) => {
+    if (item > maior) {
+      maior = item;
+    } else if (item < menor) {
+      menor = item;
     }
-
-    listaNumeros.push(valor)
-    listavisual.innerText = listaNumeros 
-    input.value = ''
-})
-
-const botaoAnalisar = document.querySelector('#btn-analisar')
-
-botaoAnalisar.addEventListener('click', () => {
-
-    if(listaNumeros.length === 0){
-        alert('Lista vazia')
-        return
-    }
-
-    let maior = listaNumeros[0]
-    let menor = listaNumeros[0]
-
-    listaNumeros.forEach((item) => {
-        
-        if (item > maior) {
-            maior = item 
-        } else if (item < menor){
-            menor = item
-        }
-    })
-    spanMaior.innerText = maior
-    spanMenor.innerText = menor
-})
+  });
+  spanMaior.innerText = maior;
+  spanMenor.innerText = menor;
+});
