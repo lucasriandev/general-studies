496c58a68026d19e8e6c9c30e68ad018c6e0d674
Lucas <lucasrian.dev@gmail.com>
2026-02-04 15:10:15 -0300

    localStorage em BarracaFrutas

--- BarracaFrutas/script.js
+++ BarracaFrutas/script.js
@@ -1,9 +1,16 @@
 const input = document.querySelector("#qtd");
 const total = document.querySelector("#total");
 
+let numeroAtual = Number(localStorage.getItem("NumeroAtual"));
+if (numeroAtual) {
+  input.value = numeroAtual;
+}
+
 function calcularTotal() {
   const valor = Number(input.value);
   const multiplicaçao = valor * 2;
+
+  localStorage.setItem("NumeroA", valor);
   console.log(multiplicaçao);
   total.innerHTML = `Total é R$${multiplicaçao}`;
 }
