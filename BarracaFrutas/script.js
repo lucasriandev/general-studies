900de0e0f3b7727ad816150104d942e69f4e7e40
Lucas <lucasrian.dev@gmail.com>
2026-02-04 15:27:38 -0300

    Barraca de frutas ajustado e finalizado

--- BarracaFrutas/script.js
+++ BarracaFrutas/script.js
@@ -1,16 +1,24 @@
 const input = document.querySelector("#qtd");
 const total = document.querySelector("#total");
 
-let numeroAtual = Number(localStorage.getItem("NumeroAtual"));
+let numeroAtual = JSON.parse(localStorage.getItem("NumeroA"));
+
 if (numeroAtual) {
-  input.value = numeroAtual;
+  input.value = numeroAtual.quantidade;
+  total.innerText = `Total é R$${numeroAtual.total}`;
 }
 
 function calcularTotal() {
   const valor = Number(input.value);
   const multiplicaçao = valor * 2;
 
-  localStorage.setItem("NumeroA", valor);
+  const valores = {
+    quantidade: valor,
+    total: multiplicaçao,
+  };
+
+  localStorage.setItem("NumeroA", JSON.stringify(valores));
+
   console.log(multiplicaçao);
   total.innerHTML = `Total é R$${multiplicaçao}`;
 }
