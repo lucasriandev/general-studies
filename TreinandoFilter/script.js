8c3917b2f393ab9e63e405752f53dbf1d69be269
Lucas <lucasrian.dev@gmail.com>
2026-02-10 18:55:05 -0300

    Treinando filter

---
+++ TreinandoFilter/script.js
@@ -0,0 +1,49 @@
+const input = document.querySelector("#input-tarefa");
+const btnAdd = document.querySelector("#btn-add");
+const btnLimpar = document.querySelector("#btn-limpar");
+const lista = document.querySelector("#lista");
+
+let minhasTarefas = JSON.parse(localStorage.getItem("MinhasTarefas")) || [];
+minhasTarefas = Array.isArray(minhasTarefas) ? minhasTarefas : [];
+
+function salvar() {
+  const valor = input.value;
+  let listaTarefas = {
+    tarefa: valor,
+    concluida: false,
+  };
+
+  minhasTarefas.push(listaTarefas);
+  localStorage.setItem("MinhasTarefas", JSON.stringify(minhasTarefas));
+  input.value = "";
+  renderizar();
+  console.log(minhasTarefas);
+}
+
+function renderizar() {
+  lista.innerHTML = "";
+  minhasTarefas.forEach((item) => {
+    const novoLi = document.createElement("li");
+    if (item.concluida === true) {
+      novoLi.classList.add("riscado");
+    }
+    novoLi.innerHTML = `<span>${item.tarefa}</span>`;
+    novoLi.addEventListener("click", () => {
+      item.concluida = !item.concluida;
+      localStorage.setItem("MinhasTarefas", JSON.stringify(minhasTarefas));
+      renderizar();
+      input.value;
+    });
+    lista.appendChild(novoLi);
+  });
+}
+
+btnLimpar.addEventListener("click", () => {
+  minhasTarefas = minhasTarefas.filter((item) => item.concluida === false);
+  localStorage.setItem("MinhasTarefas", JSON.stringify(minhasTarefas));
+  renderizar();
+});
+
+renderizar();
+
+btnAdd.addEventListener("click", salvar);
