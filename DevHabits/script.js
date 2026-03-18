48af333a84bfc159adc039038489aa52e1cdc1e6
Lucas <lucasrian.dev@gmail.com>
2026-03-18 18:54:30 -0300

    projeto em andamento

---
+++ DevHabits/script.js
@@ -0,0 +1,74 @@
+const form = document.querySelector("#form-habito");
+const input = document.querySelector("#input-tarefa");
+
+const listaPendentes = document.querySelector("#lista-pendentes");
+const listaConcluido = document.querySelector("#lista-concluidos");
+
+const tarefas = JSON.parse(localStorage.getItem("Atual")) || [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const valor = input.value;
+
+  const habitos = {
+    id: Date.now(),
+    texto: valor,
+    status: "pendente",
+  };
+
+  tarefas.push(habitos);
+  localStorage.setItem("Atual", JSON.stringify(tarefas));
+  renderizar();
+
+  input.value = "";
+});
+
+console.log(tarefas);
+
+function renderizar() {
+  listaConcluido.innerHTML = "";
+  listaPendentes.innerHTML = "";
+
+  tarefas.forEach((item, index) => {
+    const novoLi = document.createElement("li");
+    novoLi.innerHTML = item.texto;
+    novoLi.classList.add("tarefa");
+
+    if (item.status === "pendente") {
+      const btnConcluir = document.createElement("button");
+      btnConcluir.innerText = "Concluir";
+      btnConcluir.classList.add("btn-acao", "btn-concluir");
+
+      btnConcluir.addEventListener("click", () => {
+        const concluidos = tarefas.find((t) => t.id === item.id);
+        //"Me dá exatamente o item que tem esse ID"
+        concluidos.status = "concluido";
+        localStorage.setItem("Atual", JSON.stringify(tarefas));
+        renderizar();
+      });
+
+      novoLi.appendChild(btnConcluir);
+      listaPendentes.appendChild(novoLi);
+    }
+
+    if (item.status === "concluido") {
+      novoLi.classList.add("feita");
+      const btnDesfazer = document.createElement("button");
+      btnDesfazer.innerHTML = "Desfazer";
+      btnDesfazer.classList.add("btn-acao", "btn-desfazer");
+
+      btnDesfazer.addEventListener("click", () => {
+        const desfaz = tarefas.find((i) => i.id === item.id);
+        desfaz.status = "pendente";
+        localStorage.setItem("Atual", JSON.stringify(tarefas));
+        renderizar();
+      });
+
+      novoLi.appendChild(btnDesfazer);
+      listaConcluido.appendChild(novoLi);
+    }
+  });
+}
+
+renderizar();
